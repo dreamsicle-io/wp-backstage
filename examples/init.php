e1aa031918d1663cf194e66b32b4292a65d76e4b
Noah Mason <noah@dreamsicle.io>
2018-07-31 18:06:36 -0400

    Sets up portfolio example.

--- examples/init.php
+++ examples/init.php
@@ -1,11 +1,28 @@
 <?php
 
-require get_template_directory() . '/includes/wp-cpt-factory/class-wp-cpt';
+/**
+ * Require the class
+ */
+require get_stylesheet_directory() . '/includes/wp-cpt-factory/class-wp-cpt.php';
 
+/**
+ * WPCPT Init
+ *
+ * Initialize all custom post types in this
+ * function, which is then called by the 
+ * `after_setup_theme` hook. 
+ */
 function wpcpt_init() {
 
-	WP_CPT::add( array(
-		
+	WP_CPT::add( 'project', array(
+		'menu_name'     => __( 'Portfolio', 'wpcpt' ), 
+		'singular_name' => __( 'Project', 'wpcpt' ), 
+		'plural_name'   => __( 'Projects', 'wpcpt' ), 
+		'description'   => __( 'A portfolio of projects.', 'wpcpt' ), 
+		'singular_base' => 'project', 
+		'archive_base'  => 'projects', 
+		'rest_base'     => 'projects', 
+		'hierarchical'  => true, 
 	) );
 
 }

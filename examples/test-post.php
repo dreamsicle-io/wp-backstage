e9072e44bca9bec64c591ba480ffdb6c80dc1b66
Noah Mason <noah@dreamsicle.io>
2018-08-02 05:00:45 -0400

    Use only one example for dev.

---
+++ examples/test-post.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * WPCPT Init
+ *
+ * Initialize all custom post types in this
+ * function, which is then called by the 
+ * `after_setup_theme` hook. 
+ */
+function wpcpt_init_default() {
+
+	WP_CPT::add( 'wpcpt_test_post', array(
+		'menu_name'      => __( 'Test Posts', 'wpcpt' ), 
+		'singular_name'  => __( 'Test Post', 'wpcpt' ), 
+		'plural_name'    => __( 'Test Posts', 'wpcpt' ), 
+		'description'    => __( 'Testing all fields.', 'wpcpt' ), 
+		'singular_base'  => 'test-post', 
+		'archive_base'   => 'test-posts', 
+		'rest_base'      => 'test-posts', 
+		'group_meta_key' => 'wpcpt_test_post_meta',
+		'meta_boxes'     => array(
+			array(
+				'id'          => 'wpcpt_test_post_fields', 
+				'title'       => __( 'All Fields', 'wpcpt' ), 
+				'description' => __( 'These extra meta fields control further details about the test post. <a href="#">Example Link</a>', 'wpcpt' ), 
+				'context'     => 'normal', 
+				'priority'    => 'high', 
+				'fields'      => array(
+					array( 
+						'type'        => 'text', 
+						'name'        => 'wpcpt_text_field', 
+						'label'       => __( 'Text field', 'wpcpt' ), 
+						'description' => __( 'Please enter some text.', 'wpcpt' ), 
+						'has_column'  => true, 
+						'is_sortable' => true, 
+					),
+					array( 
+						'type'        => 'number', 
+						'name'        => 'wpcpt_number_field', 
+						'label'       => __( 'Number field', 'wpcpt' ), 
+						'description' => __( 'Please enter a number from 0-100.', 'wpcpt' ), 
+						'has_column'  => true, 
+						'is_sortable' => true, 
+						'input_attrs' => array(
+							'min'         => 0,
+							'max'         => 100,
+							'step'        => 1,
+						), 
+					),
+					array( 
+						'type'        => 'textarea', 
+						'name'        => 'wpcpt_textarea_field', 
+						'label'       => __( 'Textarea field', 'wpcpt' ), 
+						'description' => __( 'Please enter no more than 240 characters.', 'wpcpt' ), 
+						'has_column'  => true, 
+						'is_sortable' => true, 
+						'input_attrs' => array(
+							'maxlength' => 240, 
+						), 
+					),
+				), 
+			),
+			array(
+				'id'          => 'wpcpt_test_post_extras', 
+				'title'       => __( 'Extras', 'wpcpt' ), 
+				'description' => __( 'These extra meta fields control further details about the test post. <a href="#">Example Link</a>', 'wpcpt' ), 
+				'context'     => 'normal', 
+				'priority'    => 'high', 
+				'hidden'      => true, 
+				'fields'      => array(), 
+			),
+		), 
+	) );
+
+}
+
+add_action( 'after_setup_theme', 'wpcpt_init_default', 10 );

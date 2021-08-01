5677397333add055470fc503b354c57da68ecc2a
Noah Mason <noahmason23@gmail.com>
2021-07-31 22:35:56 -0400

    Removes commented code. Adds border-box box-sizing to address inputs.

--- includes/class-wp-backstage-post-type.php
+++ includes/class-wp-backstage-post-type.php
@@ -263,7 +263,6 @@ class WP_Backstage_Post_Type extends WP_Backstage {
 		add_action( sprintf( 'save_post_%1$s', $this->slug ), array( $this, 'save' ), 10, 3 );
 		add_filter( 'default_hidden_meta_boxes', array( $this, 'manage_default_hidden_meta_boxes' ), 10, 2 );
 		add_filter( 'default_hidden_columns', array( $this, 'manage_default_hidden_columns' ), 10, 2 );
-		// add_filter( 'edit_form_top', array( $this, 'render_edit_nonce' ), 10 );
 		add_filter( sprintf( 'manage_%1$s_posts_columns', $this->slug ), array( $this, 'add_thumbnail_column' ), 10 );
 		add_filter( sprintf( 'manage_%1$s_posts_columns', $this->slug ), array( $this, 'add_field_columns' ), 10 );
 		add_action( sprintf( 'manage_%1$s_posts_custom_column', $this->slug ), array( $this, 'render_admin_column' ), 10, 2 );

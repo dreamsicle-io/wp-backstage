1269940502d393fc24e36f9fcec9c03d017d8ae9
Noah Mason <noahmason23@gmail.com>
2022-11-23 09:48:18 -0500

    Bumps version.

--- wp-backstage.php
+++ wp-backstage.php
@@ -8,7 +8,7 @@
  * Plugin Name:       WP Backstage
  * Plugin URI:        https://github.com/dreamsicle-io/wp-backstage
  * Description:       Create standardized and deployable WordPress objects like post types with meta boxes and custom fields, taxonomy terms with custom fields, options pages with custom fields and widgets with custom fields; and extend existing WordPress objects like pages, posts, attachments, categories, tags, users and nav menu items.
- * Version:           3.4.0
+ * Version:           3.4.1
  * Requires PHP:      7.4.0
  * Requires at least: 5.8.0
  * Author:            Dreamsicle

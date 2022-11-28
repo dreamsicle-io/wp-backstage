3fbf06f38abe8c8b0cb799eafce42ce30878fc6f
Noah Mason <noahmason23@gmail.com>
2022-11-28 10:51:01 -0500

    Adds test widget render function.

--- examples/tests.php
+++ examples/tests.php
@@ -841,3 +841,12 @@ function wp_backstage_init() {
 }
 
 add_action( 'after_setup_theme', 'wp_backstage_init', 10 );
+
+function wp_backstage_test_widget( $args = array(), $instance = array() ) { ?>
+	<textarea readonly rows="20" style="display:block;margin:0;white-space:pre;max-width:100%;max-height:400px;overflow:auto;font-size:10px;background-color:rgba(0, 0, 0, 0.05);padding:4px 8px;box-sizing:border-box;min-width:0;"><?php
+		// phpcs:ignore WordPress.PHP.DevelopmentFunctions
+		print_r( $instance );
+	?></textarea>
+<?php }
+
+add_action( 'wp_backstage_widget_output_wp_backstage_widget', 'wp_backstage_test_widget', 10, 2 );
